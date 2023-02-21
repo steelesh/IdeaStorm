adcc251a9774146ab3897da36912e2bb28f8afe0
Steele <shrevesa@mail.uc.edu>
2023-02-20 19:30:28 -0500

    Create unit test class to test activity data

---
+++ app/src/test/java/com/ideastorm/v25001/ActivityTests.kt
@@ -0,0 +1,10 @@
+package com.ideastorm.v25001
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+class ActivityTests {
+    @get:Rule
+    var rule:TestRule = InstantTaskExecutorRule()
+}
