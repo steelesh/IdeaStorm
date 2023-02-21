06ecbcf6998255cc953e894495ef47c9451fa240
Steele <shrevesa@mail.uc.edu>
2023-02-20 20:29:15 -0500

    Create navigation bar UI element

--- app/src/main/java/com/ideastorm/v25001/MainActivity.kt
+++ app/src/main/java/com/ideastorm/v25001/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.ideastorm.v25001
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ideastorm.v25001.ui.theme.IdeaStormTheme
 
@@ -22,22 +24,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
+                OptionMenu("IdeaStorm")
             }
         }
     }
 }
 
+/**
+ * Creates a TopAppBar with the app title on the left and a navigation menu on the right
+ * @author Steele Shreve
+ * @param appName name of the application
+ */
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OptionMenu(appName: String) {
+    var showMenu by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    TopAppBar(
+        title = { Text(appName)},
+        actions = {
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(Icons.Default.MoreVert, "")
+            }
+            DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false}) {
+                DropdownMenuItem(onClick = { Toast.makeText(context, "Account", Toast.LENGTH_SHORT).show() }) {
+                    Text(text = "Account")
+                }
+                DropdownMenuItem(onClick = { Toast.makeText(context, "Exit", Toast.LENGTH_SHORT).show() }) {
+                    Text(text = "Exit")
+                }
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IdeaStormTheme {
-        Greeting("Android")
+        OptionMenu("IdeaStorm")
     }
 }
