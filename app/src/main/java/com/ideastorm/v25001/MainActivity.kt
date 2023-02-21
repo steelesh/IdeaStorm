7284d1a60d0827dd4a438cbc92715c7bad8b3d4b
Steele <shrevesa@mail.uc.edu>
2023-02-20 22:24:55 -0500

    Refactor navigation background color and icon

--- app/src/main/java/com/ideastorm/v25001/MainActivity.kt
+++ app/src/main/java/com/ideastorm/v25001/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ideastorm.v25001.ui.theme.IdeaStormTheme
@@ -19,7 +20,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IdeaStormTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -41,10 +41,12 @@ fun OptionMenu(appName: String) {
     var showMenu by remember { mutableStateOf(false) }
     val context = LocalContext.current
     TopAppBar(
+        modifier = Modifier.fillMaxWidth(),
         title = { Text(appName)},
+        backgroundColor = Color(android.graphics.Color.parseColor("#D9D9D9")),
         actions = {
             IconButton(onClick = { showMenu = !showMenu }) {
-                Icon(Icons.Default.MoreVert, "")
+                Icon(Icons.Default.Menu, "Navigation")
             }
             DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false}) {
                 DropdownMenuItem(onClick = { Toast.makeText(context, "Account", Toast.LENGTH_SHORT).show() }) {
