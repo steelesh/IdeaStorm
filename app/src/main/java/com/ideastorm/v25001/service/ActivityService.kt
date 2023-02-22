8a18db3025785877aabd364abb505d34c3fe6026
Steele <shrevesa@mail.uc.edu>
2023-02-22 03:20:28 -0500

    Add fetchActivity() function to service layer

--- app/src/main/java/com/ideastorm/v25001/service/ActivityService.kt
+++ app/src/main/java/com/ideastorm/v25001/service/ActivityService.kt
@@ -1,3 +1,20 @@
 package com.ideastorm.v25001.service
 
-class ActivityService
+import com.ideastorm.v25001.RetrofitClientInstance
+import com.ideastorm.v25001.dao.IActivityDAO
+import com.ideastorm.v25001.dto.Activity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import retrofit2.awaitResponse
+
+class ActivityService {
+    suspend fun fetchActivity() : Activity {
+        return withContext(Dispatchers.IO) {
+            val service = RetrofitClientInstance.retrofitInstance?.create(IActivityDAO::class.java)
+            val activity = async { service?.getActivity() }
+            var result = activity.await()?.awaitResponse()?.body()
+            return@withContext result!!
+        }
+    }
+}
