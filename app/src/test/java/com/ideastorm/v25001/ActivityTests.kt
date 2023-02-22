0b1b7bd6b5f20c86c0d8e357f02e126ca40d799d
Logan Conley <conleylp@mail.uc.edu>
2023-02-22 05:36:23 -0800

    Created first UnitTest

--- app/src/test/java/com/ideastorm/v25001/ActivityTests.kt
+++ app/src/test/java/com/ideastorm/v25001/ActivityTests.kt
@@ -1,10 +1,19 @@
 package com.ideastorm.v25001
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
 
 class ActivityTests {
+
     @get:Rule
-    var rule:TestRule = InstantTaskExecutorRule()
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Test
+    fun 'Given activity data is available when I specifcy 3 participants then I should receive an activity that can be dones with three people'() {
+        var activity = Activity(participants: 3)
+        Assert.assertTrue(activity.participants.equals(3))
+    }
 }
