9101ff9fd7585ca608d8f632c30027778f6378de
Logan Conley <conleylp@mail.uc.edu>
2023-02-22 05:41:11 -0800

    Created UnitTest for example 1.2

--- app/src/test/java/com/ideastorm/v25001/ActivityTests.kt
+++ app/src/test/java/com/ideastorm/v25001/ActivityTests.kt
@@ -1,5 +1,6 @@
 package com.ideastorm.v25001
 
+import android.app.Activity
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert
 import org.junit.Rule
@@ -15,5 +16,11 @@ class ActivityTests {
     fun 'Given activity data is available when I specifcy 3 participants then I should receive an activity that can be dones with three people'() {
         var activity = Activity(participants: 3)
         Assert.assertTrue(activity.participants.equals(3))
+    }
+
+    @Test
+    fun 'Given activity data is available when I specifcy the social type then I should receive an activity that is social'() {
+        var activity = Activity(type: "social")
+        Assert.assertTrue(activity.participants.equals("social"))
     }
 }
