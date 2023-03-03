28811b4a3321a1761611ec8f912c486582e18fc6
Steele <shrevesa@mail.uc.edu>
2023-03-02 21:46:10 -0500

    Declare firestore variable and import into MainViewModel

--- app/src/main/java/com/ideastorm/v25001/MainViewModel.kt
+++ app/src/main/java/com/ideastorm/v25001/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.ideastorm.v25001
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ideastorm.v25001.dto.Activity
 import com.ideastorm.v25001.service.ActivityService
 import com.ideastorm.v25001.service.IActivityService
@@ -10,6 +11,8 @@ import kotlinx.coroutines.launch
 
 class MainViewModel(var activityService : IActivityService = ActivityService()) : ViewModel() {
     var activity : MutableLiveData<Activity> = MutableLiveData<Activity>()
+
+    private lateinit var firestore : FirebaseFirestore
 
     fun fetchActivity() {
         viewModelScope.launch {
