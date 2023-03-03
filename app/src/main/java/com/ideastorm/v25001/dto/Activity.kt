291c33aa783a970b0314d9ffa32bc154665df532
gabriel <comstogm@mail.uc.edu>
2023-03-02 19:50:19 -0500

    comstogm Code Review 1 suggestions

--- app/src/main/java/com/ideastorm/v25001/dto/Activity.kt
+++ app/src/main/java/com/ideastorm/v25001/dto/Activity.kt
@@ -2,13 +2,20 @@ package com.ideastorm.v25001.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Activity(@SerializedName("activity") var activity: String,
-                    @SerializedName("accessibility") var accessibility: Float,
-                    @SerializedName("type") var type: String,
-                    @SerializedName("participants") var participants: Int,
-                    @SerializedName("price") var price: Float,
-                    @SerializedName("link") var link: String,
-                    @SerializedName("key") var key: Long) {
+data class Activity(@SerializedName("activity")
+                    var activity: String,
+                    @SerializedName("accessibility")
+                    var accessibility: Float,
+                    @SerializedName("type")
+                    var type: String,
+                    @SerializedName("participants")
+                    var participants: Int,
+                    @SerializedName("price")
+                    var price: Float,
+                    @SerializedName("link")
+                    var link: String,
+                    @SerializedName("key")
+                    var key: Long) {
     override fun toString(): String {
         return activity
     }
