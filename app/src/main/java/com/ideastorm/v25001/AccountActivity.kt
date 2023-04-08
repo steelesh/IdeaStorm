e4125c1b9b04981184ef0b182b880ee408bc16a2
Steele Shreve <shrevesa@mail.uc.edu>
2023-04-08 15:27:18 -0400

    Create basic skeleton for Account page for logged in users

--- app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
+++ app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
@@ -1,5 +1,30 @@
 package com.ideastorm.v25001
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ideastorm.v25001.ui.theme.IdeaStormTheme
 
-class AccountActivity : ComponentActivity()
+class AccountActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IdeaStormTheme {
+                ProfileScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun ProfileScreen() {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("User Profile")
+        }
+    }
+}
