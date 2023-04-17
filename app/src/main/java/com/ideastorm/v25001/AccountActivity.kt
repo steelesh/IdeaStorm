84235c01266fc1e6dbbb0a7f8ba7ee35010ba730
Steele <shrevesa@mail.uc.edu>
2023-04-17 17:53:52 -0400

    Fetch user information and display it inside ProfileScreen composable

--- app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
+++ app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
@@ -3,7 +3,16 @@ package com.ideastorm.v25001
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.ideastorm.v25001.ui.theme.IdeaStormTheme
@@ -20,5 +29,28 @@ class AccountActivity : ComponentActivity() {
     }
     @Composable
     fun ProfileScreen() {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            firebaseUser?.let { user ->
+                user.displayName?.let { name ->
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h4,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                user.email?.let { email ->
+                    Text(
+                        text = email,
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
     }
 }
