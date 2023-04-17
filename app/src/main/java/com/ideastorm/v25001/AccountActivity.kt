0ee13f80acb9003ff53fc4a2de415d68864e28a6
Steele <shrevesa@mail.uc.edu>
2023-04-17 17:51:52 -0400

    Declare logged in user in variable firebaseUser

--- app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
+++ app/src/main/java/com/ideastorm/v25001/AccountActivity.kt
@@ -3,15 +3,13 @@ package com.ideastorm.v25001
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.ideastorm.v25001.ui.theme.IdeaStormTheme
 
 class AccountActivity : ComponentActivity() {
+    var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,11 +18,7 @@ class AccountActivity : ComponentActivity() {
             }
         }
     }
-
     @Composable
     fun ProfileScreen() {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("User Profile")
-        }
     }
 }
