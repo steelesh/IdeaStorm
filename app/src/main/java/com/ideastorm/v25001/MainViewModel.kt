cae2495c5805d2f9374a69c7ca3919b41720829a
CrayarC52 <conleylp@mail.uc.edu>
2023-04-21 15:07:56 -0400

    Add in fetchActivity function with no parameters so old Activity test works

--- app/src/main/java/com/ideastorm/v25001/MainViewModel.kt
+++ app/src/main/java/com/ideastorm/v25001/MainViewModel.kt
@@ -3,12 +3,14 @@ package com.ideastorm.v25001
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.ideastorm.v25001.dto.Activity
 import com.ideastorm.v25001.dto.User
 import com.ideastorm.v25001.service.ActivityService
 import com.ideastorm.v25001.service.IActivityService
+import kotlinx.coroutines.launch
 
 class MainViewModel(var activityService: IActivityService = ActivityService()) : ViewModel() {
     var activity: MutableLiveData<Activity> = MutableLiveData<Activity>()
@@ -20,6 +22,13 @@ class MainViewModel(var activityService: IActivityService = ActivityService()) :
     init {
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
+
+    fun fetchActivity() {
+        viewModelScope.launch {
+            var innerActivity = activityService.fetchActivity()
+            activity.postValue(innerActivity)
+        }
     }
 
     fun fetchActivity(
